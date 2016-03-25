0171369a6e5fb188d4f8668e68544eb5f1377c0a
jakzaizzat <jakzaizzat@gmail.com>
2016-03-25 16:30:59 +0800

    Added Raty for Ratting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325070903) do
+ActiveRecord::Schema.define(version: 20160325074703) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -30,8 +30,9 @@ ActiveRecord::Schema.define(version: 20160325070903) do
     t.integer  "user_id"
     t.integer  "place_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "score",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
