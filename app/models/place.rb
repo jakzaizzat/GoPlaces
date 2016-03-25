d15fd13552a7bfc43611fb981d1485e8fb74d0c2
jakzaizzat <jakzaizzat@gmail.com>
2016-03-25 15:03:57 +0800

    Added map to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,4 +8,12 @@ class Place < ActiveRecord::Base
 
 	#Association with User
 	belongs_to :user
+
+
+	geocoded_by :address
+	after_validation :geocode
+
+	reverse_geocoded_by :latitude, :longitude
+	after_validation :reverse_geocode  # auto-fetch address
+
 end
